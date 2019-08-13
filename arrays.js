9f984cddcd286d5577907834346c5e30bf5a6ff1
Amanda R <abrolon87@gmail.com>
2019-08-13 05:42:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 }
 
 function removeElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars.slice(0, chocolateBars.length -1);
 }
